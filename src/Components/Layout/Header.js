c0eeb96157e823be428abde97503b5cdd353b23c
uwishema11 <uwishemaceline4@gmail.com>
2023-04-13 17:48:04 +0200

    meal app

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header =props =>{
     return (
         <React.Fragment>
             <header className ={classes.header}>
-                <h1>UCMeals</h1>
+                <h1>platePal</h1>
                 <HeaderCartButoon onClick={props.onClick}/>
             </header>
             <div className={classes['main-image']}>
